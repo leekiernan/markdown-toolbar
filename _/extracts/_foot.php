e145687667f099d7372242f22dfd16039d6a5283
James Crockford <jmscrckfrd@gmail.com>
2013-06-02 10:56:07 +0100

    removed can versions of jQuery - faster to prepend, commented out GA for development, absolute linking

--- _/extracts/_foot.php
+++ _/extracts/_foot.php
@@ -1,17 +1,11 @@
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-        <!--[if lt IE 9]>
-            <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <![endif]-->
+        <script src="/_/script/js/main.min.js"></script>
 
-        <!-- <script>window.jQuery || document.write('<script src="_/script/js/vendor/jquery-1.9.1.min.js"><\/script>')</script> -->
-        <script src="_/script/js/main.js"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID.
         <script>
             var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
             (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
             g.src='//www.google-analytics.com/ga.js';
             s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
+        </script>-->
     </body>
 </html>
