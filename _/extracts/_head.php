147704f6045b5d389a2ae69c80bd891d442209c8
James Crockford <jmscrckfrd@gmail.com>
2013-06-02 10:54:45 +0100

    meta viewport updated for responsive sites, use single stylesheet, use absolute linking

--- _/extracts/_head.php
+++ _/extracts/_head.php
@@ -8,15 +8,13 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title></title>
         <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-
-        <link rel="stylesheet" href="_/css/normalize.css">
-        <link rel="stylesheet" href="_/css/screen.css">
-        <script src="_/script/js/vendor/modernizr-2.6.2.min.js"></script>
+        <link rel="stylesheet" href="/_/css/screen.css">
+        <script src="/_/script/js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 9]>
-            <p class="alert">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+            <style>body{padding-top:0!important;margin-top:0!important;} .chromeframe {font-family: "Microsoft Sans Serif", sans-serif; background: #ffffe0; color: #000; border-bottom: solid 1px #a9a495; padding: 1em;}</style>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
